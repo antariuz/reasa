8b2835b5a5eb277af5e7f225e150c0b264521a26
black <antariuz@gmail.com>
2023-01-04 18:52:11 +0200

    Initial checks class added

---
+++ src/main/java/avada/spacelab/reasa/data/Init.java
@@ -0,0 +1,14 @@
+package avada.spacelab.reasa.data;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Init implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) {
+        //  initial checks
+    }
+
+}
