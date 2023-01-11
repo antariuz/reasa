4e126e75c5444fab643c072821a13e8811e07d77
black <antariuz@gmail.com>
2023-01-11 17:03:20 +0200

    Added Location model

---
+++ src/main/java/avada/spacelab/reasa/model/Location.java
@@ -0,0 +1,23 @@
+package avada.spacelab.reasa.model;
+
+import avada.spacelab.reasa.model.common.MappedEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@Data
+@EqualsAndHashCode(callSuper = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class Location extends MappedEntity {
+
+    private double latitude;
+    private double longitude;
+
+}
